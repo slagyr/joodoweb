8c369bf1b0cbdf9e410287f4d4fb2e33c26981c2
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-11-21 14:21:54 -0600

    Minor fix

--- src/joodoweb/view/docs/fn_doc.hiccup.clj
+++ src/joodoweb/view/docs/fn_doc.hiccup.clj
@@ -1,8 +1,10 @@
-(let [ns-info (meta (:joodo-ns *view-context*))
-	  fn-info (meta (:joodo-fn *view-context*))]
+(let [ns-info (ns-name (:joodo-ns *view-context*))
+      fn-info (meta (:joodo-fn *view-context*))]
   (list
-	[:a {:href (str "/docs/" (:name ns-info))} (:name ns-info)]
     [:h2 (:name fn-info)]
-    [:p (:doc fn-info)]
+    [:a {:href (str "/docs/" (ns->url (ns-name ns-info)))} (str "Back to " (ns-name ns-info))]
+    (if (= nil (:doc fn-info))
+      [:p "Documentation not found"]
+      [:p (:doc fn-info)])
   )
 )
