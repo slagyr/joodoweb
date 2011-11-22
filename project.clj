8ec73c64235c77795e2fe3912c821f174431c735
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-11-22 10:30:46 -0600

    Only shows functions that have documentation.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject joodoweb "0.0.1"
   :description "A website deployable to AppEngine"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [joodo "0.6.0-SNAPSHOT"]]
+                 [org.clojars.cuvuligio/joodo "0.6.1-SNAPSHOT"]]
   :dev-dependencies [[speclj "1.4.0"]]
   :test-path "spec/"
   :java-source-path "src/"
--- src/joodoweb/view/docs/ns_doc.hiccup.clj
+++ src/joodoweb/view/docs/ns_doc.hiccup.clj
@@ -3,6 +3,9 @@
   (list
     [:h2 ns-name]
     [:ul
-      (map #(merge [:li [:a {:href (str "/docs/" (ns->url ns-name) "/" (fn->url (first %)))} (first %)]]) (ns-publics joodo-ns))]
+      (map
+	    #(if (not= nil (:doc (meta (second %))))
+	      (merge [:li [:a {:href (str "/docs/" (ns->url ns-name) "/" (fn->url (first %)))} (first %)]]))
+        (ns-publics joodo-ns))]
   )
 )
