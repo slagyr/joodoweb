704443e5be8d7cd0d0f6a952b20f85e1dddfc90b
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-11-28 09:28:25 -0600

    Added some content and spellchecked the entire website.

--- src/joodoweb/view/tutorial/tour.hiccup.clj
+++ src/joodoweb/view/tutorial/tour.hiccup.clj
@@ -1,5 +1,5 @@
 [:h2 "The Grand Tour"]
-[:p "Because of the amount of functionality that Joodo comes with out of the box, understaning how everthing works can be a little overwhelming if it is your first time seeing a Joodo project. This section will give an overview of the important parts of any Joodo project."]
+[:p "Because of the amount of functionality that Joodo comes with out of the box, understanding how everything works can be a little overwhelming if it is your first time seeing a Joodo project. This section will give an overview of the important parts of any Joodo project."]
 
 [:h3 "Project.clj"]
 [:p "The heart of any Leiningen project is the project.clj file. This file lists information about your project. You can add descriptions, change version numbers, add dependencies, and much more in this file. More info about the project.clj file can be found on the " [:a {:href "https://github.com/technomancy/leiningen/blob/master/README.md" :target "_blank"} "latest tutorial"] " on Leiningen's github account."]
@@ -7,7 +7,7 @@
 [:h3 "Core.clj"]
 [:p "The next most important file is the core.clj file buried in your src/ directory. By default there are three sections of core.clj. It is important to keep in mind that you can modify/add sections to fit your project's needs. These are just there to get you started."]
 [:p "The first section declares the file's namespace and lists all of the file's dependencies. If you want a deeper look into what core.clj does you can find the method/macro definitions that are being used in this section."]
-[:p "The second section calls a macro called defroutes. This macro is responsable for defining the routes of the website. By default it sets a GET request on the '/' route to render a pre-made index page. It also tells the application to render a pre-made 404 page if a non-existent route is accessed."]
+[:p "The second section calls a macro called defroutes. This macro is responsible for defining the routes of the website. By default it sets a GET request on the '/' route to render a pre-made index page. It also tells the application to render a pre-made 404 page if a non-existent route is accessed."]
 [:p "The most interesting part of the second section tells Joodo to look for any files with namespaces starting with '_project_name_.controller and add the routes they define to the list of routes. More about controllers can be found in the controllers section below."]
 [:p "The third section wraps information around the routes. By default, the only property being wrapped is the view context. It sets the template root to the view directory and sets all of the view's namespaces to a view-helper."]
 
@@ -24,4 +24,4 @@
 [:p "If you want to make these pages dynamic, you can write clojure logic directly in the hiccup file. If you want to limit the amount of logic in your views you can extract logic into your view_helpers.clj file and access those functions in all of your views."]
 
 [:h3 "Helper Functions"]
-[:p "Joodo supplies many functions that handle the most common behaviours of a web application. They were built specifically for controllers, models, views, and tests. The best way to get familiar with these type of functions is to dive into the " [:a {:href "/docs"} "documentation"] "."]
+[:p "Joodo supplies many functions that handle the most common behaviors of a web application. They were built specifically for controllers, models, views, and tests. The best way to get familiar with these type of functions is to dive into the " [:a {:href "/docs"} "documentation"] "."]
