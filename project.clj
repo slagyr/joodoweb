6d1220dcded269c1b2412b69bc07dd477b99d99b
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-12-02 10:47:09 -0600

    Fixed the documentation in production bug.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject joodoweb "0.0.1"
   :description "A website deployable to AppEngine"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [joodo "0.6.3-SNAPSHOT"]]
+                 [joodo "0.6.3-SNAPSHOT"]
+                 [speclj "1.4.0"]]
   :dev-dependencies [[speclj "1.4.0"]]
   :test-path "spec/"
   :java-source-path "src/"
