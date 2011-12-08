fa8f1481bbcd1613097e80ffac55db3384a988a1
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-12-08 11:44:11 -0600

    Moved content to the index page.

--- src/joodoweb/view/index.hiccup.clj
+++ src/joodoweb/view/index.hiccup.clj
@@ -1,51 +1,25 @@
-[:h2 "joodo"]
 [:h3 "a web application framework for Clojure"]
 
 [:br]
 [:p "Born of a need to build fast, clean web applications, Joodo uniquely satisfies a need of Clojure frameworks. Taking inspiration from the self-discipline of martial arts and the attention to detail of Old World artisans, Joodo is a carefully crafted tool that will aid you in forging the best applications you can imagine."]
 [:p "Joodo is an open-source project, free for all, and constantly updated."]
-; [:pre "joodo new abc
-; 	creating file:       abc/project.clj
-; 	creating file:       abc/Procfile
-; 	creating directory:  abc/public
-; 	creating directory:  abc/public/images
-; 	creating file:       abc/public/images/joodo.png
-; 	creating directory:  abc/public/javascript
-; 	creating file:       abc/public/javascript/abc.js
-; 	creating directory:  abc/public/stylesheets
-; 	creating file:       abc/public/stylesheets/abc.css
-; 	creating directory:  abc/spec
-; 	creating directory:  abc/spec/abc
-; 	creating file:       abc/spec/abc/core_spec.clj
-; 	creating directory:  abc/src
-; 	creating directory:  abc/src/abc
-; 	creating file:       abc/src/abc/core.clj
-; 	creating directory:  abc/src/abc/controller
-; 	creating directory:  abc/src/abc/model
-; 	creating directory:  abc/src/abc/view
-; 	creating file:       abc/src/abc/view/view_helpers.clj
-; 	creating file:       abc/src/abc/view/layout.hiccup.clj
-; 	creating file:       abc/src/abc/view/index.hiccup.clj
-; 	creating file:       abc/src/abc/view/not_found.hiccup.clj
-; 	creating directory:  abc/config
-; 	creating file:       abc/config/environment.clj
-; 	creating file:       abc/config/development.clj
-; 	creating file:       abc/config/production.clj"]
-; 
-; 
-; [:span "joodo server
-; Cleaning up.
-; Exception in thread \"main\" java.lang.NoClassDefFoundError: joodo/kake/JoodoServer
-; Caused by: java.lang.ClassNotFoundException: joodo.kake.JoodoServer
-; 	at java.net.URLClassLoader$1.run(URLClassLoader.java:202)
-; 	at java.security.AccessController.doPrivileged(Native Method)
-; 	at java.net.URLClassLoader.findClass(URLClassLoader.java:190)
-; 	at java.lang.ClassLoader.loadClass(ClassLoader.java:306)
-; 	at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:301)
-; 	at java.lang.ClassLoader.loadClass(ClassLoader.java:247)"]
-; 
-; 
-; [:span "lein deps
-; [INFO] snapshot joodo:joodo:0.6.0-SNAPSHOT: checking for updates from clojars
-; Copying 17 files to /Users/dcmoore/Projects/abc/lib
-; Copying 4 files to /Users/dcmoore/Projects/abc/lib/dev"]
+
+[:h3 "Installation"]
+[:p "Installing Joodo is as easy as your favorite pie (eating, not baking): all you need is Leiningen and the Joodo plugin. Follow the " [:a {:href "https://github.com/technomancy/leiningen/blob/master/README.md" :target "_blank"} "latest tutorial"] " on Leiningen's github account to acquire the latest version, then visit your terminal to install the Joodo plugin:"]
+[:code "joodo/lein-joodo 0.6.0-SNAPSHOT"]
+[:p "The 'joodo' command will be installed on your system in the '~/.lein/bin/' directory. Next, edit the '.profile' (found in your home directory) to include '$HOME/.lein/bin/' in your path. Run the following in your terminal:"]
+[:code "export PATH=$PATH:~/.lein/bin"]
+[:p "Restart your terminal to apply these changes, and consider your pie eaten. Delicious, wasn't it?"]
+
+[:h3 "Usage"]
+[:p "Part of the beauty of Joodo is how easy it is to build a new project. If you run the following command from your terminal (replacing _proj_name_ with the name of your project), Joodo will do all the heavy lifting and create a project for you:"]
+[:code "joodo new _proj_name_"]
+[:p "By default, Joodo projects have dependencies on external libraries. Before you run your project, you have to get those libraries. Fortunately, Leiningen makes this as easy as running the following command from your terminal when in your project's directory:"]
+[:code "lein deps"]
+[:p "Now you can run your project by running the following command:"]
+[:code "joodo server"]
+
+[:h3 "Deployment"]
+[:p "If you use git and want to deploy on Heroku, pushing your project to production is as easy as creating a git repo, making a commit, then running the following commands:"]
+[:code "heroku create --stack cedar"]
+[:code "git push heroku master"]
