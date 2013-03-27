4b06d52dc42e3e2d04d55daa9742b2761e032b77
Angeleah Daidone <angeleah@angeleah.com>
2013-03-27 10:27:37 -0500

    Update documentation to reflect the current version of Joodo and the way namespaces are generated with - instead of _,update the installation page to use joodo/lein 1.1.2, use chee.datetime instead of joodo.datetime, update old :refer syntax.

--- src/joodoweb/view/tutorial/basics.hiccup.clj
+++ src/joodoweb/view/tutorial/basics.hiccup.clj
@@ -22,23 +22,23 @@
 [:h3 "The Basics"]
 [:p "The most important file in your project is the " [:b "src/root.clj"] "."] 
 [:pre {:class "brush: clojure"}
-"(ns sample_app.core
+"(ns sample-app.core
   (:require [compojure.core :only (defroutes GET)]
             [compojure.route :only (not-found)]
             [joodo.middleware.view-context :only (wrap-view-context)]
             [joodo.views :only (render-template render-html)]
             [joodo.controllers :only (controller-router)]))
 
-(defroutes sample_app-routes
+(defroutes sample-app-routes
   (GET \"/\" [] (render-template \"index\"))
   (controller-router 'sample_app.controller)
   (not-found (render-template \"not_found\" :template-root \"sample_app/view\" 
-                                          :ns `sample_app.view.view-helpers)))
+                                          :ns `sample-app.view.view-helpers)))
 
 (def app-handler
   (->
     sample_app-routes
-    (wrap-view-context :template-root \"sample_app/view\" :ns `sample_app.view.view-helpers)))"
+    (wrap-view-context :template-root \"sample_app/view\" :ns `sample-app.view.view-helpers)))"
 ]
 
 [:p "By default there are three sections of root.clj. It is important to keep in mind that you can modify/add sections to fit your project's needs. These are just there to get you started."]
